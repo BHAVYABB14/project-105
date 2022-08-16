e26ce6a18e7c453427f9793b252a345e0d778a09
BHAVYABB14 <90505667+BHAVYABB14@users.noreply.github.com>
2022-08-16 10:00:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+webcam.set({
+    width: 350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id ="image_captured" src = "'+data_uri+'"/>';
+    });
+};
+console.log("ml5 version: ",ml5.version);
+classifier = ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/c4uCIfkVl/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("MODEL LOADED SUCCESSFULLY ! ");
+};
+function check(){
+    img = document.getElementById("image_captured");
+    classifier.classify(img, gotResults);
+}
+function gotResults(error, results ) {
+    if(error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_of_objects").innerHTML = results[0].label;
+        document.getElementById("result_of_accuracy").innerHTML = results[0].confidence.tofixed(3); 
+    }
+}
